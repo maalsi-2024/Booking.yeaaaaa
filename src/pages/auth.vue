82657090335703425e9e856a0fba4cf41f39d802
Thibault <muelthibault@gmail.com>
2025-01-17 14:55:33 +0100

    push navbar and login page

--- src/pages/auth.vue
+++ src/pages/auth.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-      <div class="bg-white shadow-lg rounded-lg w-full max-w-md p-8">
+    <div class="flex items-center justify-center min-h-screen">
+      <div class="bg-white z-10 shadow-lg rounded-lg w-full max-w-md p-8">
         <h1 class="text-2xl font-semibold text-center mb-6">
           {{ isLogin ? 'Login' : 'Register' }}
         </h1>
-        
+
         <component :is="isLogin ? 'Login' : 'Register'" @switch="toggleAuthMode" />
   
         <p class="mt-4 text-sm text-center text-gray-600">
@@ -17,6 +17,9 @@
           </button>
         </p>
       </div>
+      <video class="min-w-full min-h-full absolute object-cover"
+                    src="../assets/home_video.mp4"
+                    type="video/mp4" autoplay muted loop></video>
     </div>
   </template>
   
